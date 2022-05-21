3de4cc9f6513eab26b6dc12b662a3444e514f393
Jovany1543 <christian.marrero3@gmail.com>
2022-05-21 03:01:34 +0000

    added edit user route

--- src/api/routes.py
+++ src/api/routes.py
@@ -59,6 +59,35 @@ def get_all_users():
    serialized_users = [item.serialize() for item in users]
    return jsonify(serialized_users), 200
 
+@api.route('/users/<int:user_id>/edit', methods=['PUT'])
+def editUser(user_id):
+    body = request.json # get the request body content
+    email = request.json.get('email')
+    first_name = request.json.get('first_name')
+    last_name = request.json.get('last_name')
+    age = request.json.get('age')
+    favorite_color = request.json.get('favorite_color')
+    
+    if body is None:
+        return "The request body is null", 400
+    if not email:
+        return 'You need to enter an email',400
+    if not first_name:
+        return 'You need to enter a first name',400
+    if not last_name:
+        return 'You need to enter a last name',400
+    if not age:
+        return 'You need to enter an age', 400
+    if not favorite_color:
+        return 'You need to enter a favorite color', 400
+
+    User.query.filter_by(id=user_id).update(dict(email=email, first_name=first_name, last_name=last_name, age=age, favorite_color=favorite_color))
+    db.session.commit()
+
+    updatedUsers = User.query.all()
+    serialized_users = [item.serialize() for item in updatedUsers]
+    return jsonify(serialized_users), 200
+
 @api.route('/users/<int:user_id>/delete', methods=['DELETE'])
 def delUser(user_id):
    User.query.filter_by(id=user_id).delete()
